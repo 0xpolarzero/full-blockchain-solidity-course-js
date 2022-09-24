a978c7bd29d18b49c34d8230e2a6bf38de0d5bbd
polar0 <23r0eth@gmail.com>
2022-09-24 11:47:30 +0200

    Add ethers simple storage

---
+++ Hardhat/ethers-simple-storage/encryptKey.ts
@@ -0,0 +1,21 @@
+const ethers = require('ethers');
+const fs = require('fs');
+require('dotenv').config();
+
+async function main() {
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!);
+  const encrypterJsonKey = await wallet.encrypt(
+    process.env.PRIVATE_KEY_PASSWORD!,
+    process.env.PROCESS_KEY!,
+  );
+
+  console.log(encrypterJsonKey);
+  fs.writeFileSync('./.encryptedKey.json', encrypterJsonKey);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
