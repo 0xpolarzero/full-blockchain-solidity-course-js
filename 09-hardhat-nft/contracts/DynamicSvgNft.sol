9d9b650258862a8dadcb4c984c08625ff0151617
polar0 <23r0eth@gmail.com>
2022-09-30 17:27:37 +0200

    Update dynamic svg contract

--- 09-hardhat-nft/contracts/DynamicSvgNft.sol
+++ 09-hardhat-nft/contracts/DynamicSvgNft.sol
@@ -2,11 +2,14 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "base64-sol/base64.sol";
 
 contract DynamicSvgNft is ERC721 {
     uint256 private s_tokenCounter;
     string private i_lowImageURI;
     string private i_highImageURI;
+    string private constant base64EncodedSvgPrefix =
+        "data:image/svg+xml;base64,";
 
     constructor(string memory lowSvg, string memory highSvg)
         ERC721("DynamicSvgNft", "DSN")
@@ -21,7 +24,15 @@ contract DynamicSvgNft is ERC721 {
         s_tokenCounter += 1;
     }
 
-    function svgToImageURI() public pure returns (string memory{
-        //
+    function svgToImageURI(string memory svg)
+        public
+        pure
+        returns (string memory)
+    {
+        string memory svgBase64Encoded = Base64.encode(
+            bytes(string(abi.encodePacked(svg)))
+        );
+        return
+            string(abi.encodePacked(base64EncodedSvgPrefix, svgBase64Encoded));
     }
 }
