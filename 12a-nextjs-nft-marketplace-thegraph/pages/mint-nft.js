0a10974eeab51b9df2ce809c0d14b41c566c4d94
polar0 <23r0eth@gmail.com>
2022-10-13 09:31:44 +0200

    Fix hydratation error caused by dupplicating component in mint page

--- 12a-nextjs-nft-marketplace-thegraph/pages/mint-nft.js
+++ 12a-nextjs-nft-marketplace-thegraph/pages/mint-nft.js
@@ -1,5 +1,3 @@
-import Header from '../components/Header';
-import Footer from '../components/Footer';
 import styles from '../styles/Home.module.css';
 import nftAbi from '../constants/BasicNft.json';
 import networkMapping from '../constants/networkMapping';
@@ -66,83 +64,75 @@ export default function MintNft() {
   }, [network.chainId]);
 
   return (
-    <div className='page'>
-      <header className={styles.header}>
-        <Header />
-      </header>
-
-      <main className={styles.main}>
-        <div className='mint-container content'>
-          <div className='box-container'>
-            <div className='wrapper mint-wrapper'>
-              <h1>Mint your NFT</h1>
-              <div className='instructions'>
-                <div className='caption'>Ethereum (Goerli)</div>
-                <p>
-                  <span className='highlight'>1.</span> Get some{' '}
-                  <span className='highlight bold'>Goerli ETH</span> from{' '}
-                  <a
-                    className='highlight bold'
-                    href='https://faucets.chain.link/'
-                    target={'_blank'}
-                  >
-                    Chainlink Faucets <i className='fa-solid fa-chain'></i>
-                  </a>
-                </p>
-                <div className='caption'>Polygon (Mumbai)</div>
-                <p>
-                  <span className='highlight'>1.</span> Get some{' '}
-                  <span className='highlight bold'>MATIC</span> from{' '}
-                  <a
-                    className='highlight bold'
-                    href='https://faucet.polygon.technology/'
-                    target={'_blank'}
-                  >
-                    Polygon Faucet <i className='fa-solid fa-chain'></i>
-                  </a>
-                </p>
-                <p>
-                  <span className='highlight'>2.</span>{' '}
-                  <span className='highlight bold'>Mint a NFT</span> or buy one
-                  from the marketplace.
-                </p>
-                <p>
-                  <span className='highlight'>3.</span> Then you can{' '}
-                  <span className='highlight bold'>list it</span> on the
-                  marketplace.
-                </p>
-              </div>
-              <div className='action'>
-                <Button
-                  type='primary'
-                  className='mint-btn'
-                  disabled={isDisconnected || isWrongNetwork}
-                  loading={isMintLoading || isWalletOpen}
-                  onClick={handleSubmit}
+    <main className={styles.main}>
+      <div className='mint-container content'>
+        <div className='box-container'>
+          <div className='wrapper mint-wrapper'>
+            <h1>Mint your NFT</h1>
+            <div className='instructions'>
+              <div className='caption'>Ethereum (Goerli)</div>
+              <p>
+                <span className='highlight'>1.</span> Get some{' '}
+                <span className='highlight bold'>Goerli ETH</span> from{' '}
+                <a
+                  className='highlight bold'
+                  href='https://faucets.chain.link/'
+                  target={'_blank'}
                 >
-                  Mint
-                </Button>
-                {isDisconnected ? (
-                  isWrongNetwork ? (
-                    <div className='error bold'>
-                      You are on an unsupported network. Please change to Goerli
-                      or Mumbai.
-                    </div>
-                  ) : (
-                    <div className='error bold'>
-                      You must connect your wallet to mint a NFT.
-                    </div>
-                  )
+                  Chainlink Faucets <i className='fa-solid fa-chain'></i>
+                </a>
+              </p>
+              <div className='caption'>Polygon (Mumbai)</div>
+              <p>
+                <span className='highlight'>1.</span> Get some{' '}
+                <span className='highlight bold'>MATIC</span> from{' '}
+                <a
+                  className='highlight bold'
+                  href='https://faucet.polygon.technology/'
+                  target={'_blank'}
+                >
+                  Polygon Faucet <i className='fa-solid fa-chain'></i>
+                </a>
+              </p>
+              <p>
+                <span className='highlight'>2.</span>{' '}
+                <span className='highlight bold'>Mint a NFT</span> or buy one
+                from the marketplace.
+              </p>
+              <p>
+                <span className='highlight'>3.</span> Then you can{' '}
+                <span className='highlight bold'>list it</span> on the
+                marketplace.
+              </p>
+            </div>
+            <div className='action'>
+              <Button
+                type='primary'
+                className='mint-btn'
+                disabled={isDisconnected || isWrongNetwork}
+                loading={isMintLoading || isWalletOpen}
+                onClick={handleSubmit}
+              >
+                Mint
+              </Button>
+              {isDisconnected ? (
+                isWrongNetwork ? (
+                  <div className='error bold'>
+                    You are on an unsupported network. Please change to Goerli
+                    or Mumbai.
+                  </div>
                 ) : (
-                  ''
-                )}
-              </div>
+                  <div className='error bold'>
+                    You must connect your wallet to mint a NFT.
+                  </div>
+                )
+              ) : (
+                ''
+              )}
             </div>
           </div>
         </div>
-      </main>
-
-      <Footer />
-    </div>
+      </div>
+    </main>
   );
 }
